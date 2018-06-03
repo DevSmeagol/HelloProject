125565bc41f3b401a297d71fccebb6143ab1bdfe
DevSmeagol <devsmeagol>
2018-06-03 17:32:42 +0700

    fix migration on README.md

--- database/migrations/2018_05_23_081816_create__claim_table.php
+++ database/migrations/2018_05_23_081816_create__claim_table.php
@@ -16,75 +16,74 @@ class CreateClaimTable extends Migration
         Schema::create('createClaim', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            
             //
-            $table->text('txtRepairname');//ชื่ออู่
-            $table->text('ddlInsurer');//name insurer
-            $table->text('txtCarRegNo');//เลขทะเบียน
-            $table->text('ddlCarRegProvince');//จังหวัด
-            $table->text('ddlCMFG');//ยี่ห้อ
-            $table->text('txtCModel');//รุ่น
-            $table->text('txtAccPolicyNo');//เลขกรมธรรม์
-            $table->text('txtRefClaimNo');//เลขที่เคลม
-            $table->text('txtAccClaimNo');//เลขรับแจ้ง
-            $table->text('rbStatusCar'); //สถานะจอดซ่อม
-            $table->text('txtRepContactDate');//วันที่ติดต่อซ่อม
-            $table->text('txtClientNameContact');//ชื่อผู้ตืดต่อซ่อม
-            $table->text('txtClientTelContact');//เบอร์โทรศัพท์
-            $table->text('txtClientMobileContact');//เบอร์มือถือ
-            $table->text('txtRepContactname');//ชื่อเจ้าหน้าที่อู่
-            $table->text('wuCalendarContact_txtCalendar');//วันที่ติดต่อซ่อมล่วงหน้า
-            $table->text('txtCarRepairDate');//วันที่รถเข้าซ่อม
-            $table->text('txtClientStaffNameCarRepairo');//ชื่อเจ้าหน้าที่
-            $table->text('txtClientNameCarRepair');//ชื่อผู้นำรถเข้าซ่อม
-            $table->text('txtClientIDCardCarRepair');//เลขที่บัตรประชาชน
-            $table->text('txtClientTelCarRepair');//เบอร์โทรศัพท์
-            $table->text('txtClientMobileCarRepair');//เบอร์มือถือ
-            $table->text('txtClientAddressCarRepair');//ที่อยู่
-            $table->text('txtKMCarRepair');//หมายเลขกม
+            $table->text('Repairname');//ชื่ออู่
+            $table->text('InsurerName');//name insurer
+            $table->text('carRegNo');//เลขทะเบียน
+            $table->text('carRegProvince');//จังหวัด
+            $table->text('CMFG');//ยี่ห้อ
+            $table->text('carModel');//รุ่น
+            $table->text('accPolicyNo');//เลขกรมธรรม์
+            $table->text('claimNo');//เลขที่เคลม
+            $table->text('accClaimNo');//เลขรับแจ้ง
+            $table->text('statusCar'); //สถานะจอดซ่อม
+            $table->text('contactDate');//วันที่ติดต่อซ่อม
+            $table->text('ClientNameContact');//ชื่อผู้ตืดต่อซ่อม
+            $table->text('ClientTelContact');//เบอร์โทรศัพท์
+            $table->text('ClientMobileContact');//เบอร์มือถือ
+            $table->text('RepContactname');//ชื่อเจ้าหน้าที่อู่
+            $table->text('appointmentsRepair');//วันที่ติดต่อซ่อมล่วงหน้า
+            $table->text('carRepairDate');//วันที่รถเข้าซ่อม
+            $table->text('ClientStaffNameCarRepair');//ชื่อเจ้าหน้าที่
+            $table->text('ClientNameCarRepair');//ชื่อผู้นำรถเข้าซ่อม
+            $table->text('ClientIDCardCarRepair');//เลขที่บัตรประชาชน
+            $table->text('ClientTelCarRepair');//เบอร์โทรศัพท์
+            $table->text('ClientMobileCarRepair');//เบอร์มือถือ
+            $table->text('ClientAddressCarRepair');//ที่อยู่
+            $table->text('KMCarRepair');//หมายเลขกม
             $table->text('rbOil');//ระดับเชื้อเพลิง
             $table->text('cblItemCarRepair');//อุปกรณ์ในรถ
-            $table->text('txtLossOther');//ความเสียหายอื่นๆ
-            $table->text('wuCalendarHopeFinishDate_txtCalendar');//วันที่นัดรับรถ
+            $table->text('LossOther');//ความเสียหายอื่นๆ
+            $table->text('HopeFinishDate_txtCalendar');//วันที่นัดรับรถ
             $table->text('ddlHopeFinishTime');//เวลา
-            $table->text('txtEstimateLabor');//ประมานค่าแรงซ่อม
+            $table->text('EstimateLabor');//ประมานค่าแรงซ่อม
             $table->text('ddlTypeRepair');//ประเภทงานซ่อม
             $table->text('rbPart');//รายการอะไหล่
             $table->text('wuCalendarCompleteDate_txtCalendar');//วันที่ซ่อมรถเสร็จ
-            $table->text('txtClientGetcardate');//วันที่ลูกค้ามารับรถ
-            $table->text('txtClientNameGetcar');//ชื่อผู้มารับรถ
+            $table->text('ClientGetcardate');//วันที่ลูกค้ามารับรถ
+            $table->text('ClientNameGetcar');//ชื่อผู้มารับรถ
             $table->text('lbClientIDCardGetcar');//เลขบัตรประชาชนผู้มารับรถ
-            $table->text('txtClientTelGetcar');//เบอร์ผู้มารับรถ
-            $table->text('txtClientMobileGetcar');//เบอร์มือถือผู้มารับรถ
-            $table->text('txtKMGetcar');//เลขกมตอนมารับรถ
-            $table->text('txtLINEGetcar');//lineอผู้มารับรถ
-            $table->text('txtRelation');//เกี่ยวข้องกับผู้เอาประกัน
-            $table->text('txtRepNameGetCar');//ชื่อเจ้าหน้าที่ส่งมอบรถ
+            $table->text('ClientTelGetcar');//เบอร์ผู้มารับรถ
+            $table->text('ClientMobileGetcar');//เบอร์มือถือผู้มารับรถ
+            $table->text('KMGetcar');//เลขกมตอนมารับรถ
+            $table->text('LINEGetcar');//lineอผู้มารับรถ
+            $table->text('Relation');//เกี่ยวข้องกับผู้เอาประกัน
+            $table->text('RepNameGetCar');//ชื่อเจ้าหน้าที่ส่งมอบรถ
             $table->text('rbCommentGetcar');//ผลการซ่อม
             $table->text('rbItemGetcar');//อุปกรณ์ในรถ
             
             //add User
-            $table->text('txtChassino');//เลขตัวถัง
-            $table->text('txtModelNo');//โมเดล
-            $table->text('txtEngineNo');//หมายเลขเครื่อง
+            $table->text('Chassino');//เลขตัวถัง
+            $table->text('ModelNo');//โมเดล
+            $table->text('EngineNo');//หมายเลขเครื่อง
             $table->text('DrpCarDescId');//สภาพรถ
             $table->text('drpCCL_ID');//สีรถ
             $table->text('drpCptId');//สีที่พ่นรถ
             $table->text('DrpCarIden');//รถประกันหรือคู่กรณี
-            $table->text('txtCarIdenNo');//คันที่
-            $table->text('txtDeduction');//ค่าเสียหายส่วนแรก
+            $table->text('CarIdenNo');//คันที่
+            $table->text('Deduction');//ค่าเสียหายส่วนแรก
             $table->text('DrpDeductionSrc');//การเรียกเก็บเงิน
             $table->text('drpInsurBrId');//สาขาบริษัทประกัน
-            $table->text('txtInsurBrFax');//หมายเลขแฟ็ก
+            $table->text('InsurBrFax');//หมายเลขแฟ็ก
             $table->text('DrpPolicyTypeId');//กรมธรรม์ชั้นไหน
-            $table->text('txtAccInsuredValue');//จำนวนเงินคุ้มครอง
+            $table->text('AccInsuredValue');//จำนวนเงินคุ้มครอง
             $table->text('wuCalAcc_txtCalendar');//วันที่แจ้งอุบัติเหตุ
             $table->text('wuCalOcc_txtCalendar');//วันที่เกิดอุบัติเหตุ
-            $table->text('txtDRIVER_NAME');//ชื่อผู้ขับขี่
-            $table->text('txtDRIVER_IDCARD');//เลขบัตรชาชน
-            $table->text('txtDRIVER_PASSPORTNO');//เลขใบขับขี่
-            $table->text('txtDRIVER_PHONE_NO');//เบอร์อผู้ขับขี่
-            $table->text('txtRepEstimateDays');//วันที่คาดว่าจะซ่อมเสร็จ
+            $table->text('DRIVER_NAME');//ชื่อผู้ขับขี่
+            $table->text('DRIVER_IDCARD');//เลขบัตรชาชน
+            $table->text('DRIVER_PASSPORTNO');//เลขใบขับขี่
+            $table->text('DRIVER_PHONE_NO');//เบอร์อผู้ขับขี่
+            $table->text('RepEstimateDays');//วันที่คาดว่าจะซ่อมเสร็จ
             $table->text('txtC1');//วันที่คาดว่าซ้่อมเสร็จ
             $table->text('txtC2');//วันที่คาดว่าซ้่อมเสร็จ แก้ไข
             
